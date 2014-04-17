64fb6306ae3ffd885ab7d214d623a4129952911d
Sudhakar Vasiraju <Sudhakar.Vasiraju@bazaarvoice.com>
2014-04-17 15:13:42 -0700

    Inital Commit

--- src/groovy/Timer12.groovy
+++ src/groovy/Timer12.groovy
@@ -14,3 +14,26 @@ def temp = [ new Timer( name:"sudhakar", ct:"ATL" ),
              new Timer( name:"Mark", ct:"SFO" )]
 
 temp.each { println "${it.name}  ${it.ct}" }
+
+// example2
+
+def stimer(){
+    def initTime = new java.util.Date()
+
+    return { println "${initTime} - ${new java.util.Date()}: Elapased Time ${System.currentTimeMillis() - initTime.time}" }
+}
+
+def timer = stimer()
+sleep(3000)
+
+timer()
+sleep 3000
+timer()
+
+println()
+
+
+timer = stimer()
+timer()
+sleep 3000
+timer()
